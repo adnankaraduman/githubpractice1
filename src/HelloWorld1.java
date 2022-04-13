4c134bef87537c787accd0d57a2a09efa1b4cdc8
ado <akrdmn66@gmail.com>
2022-04-13 10:14:42 +0300

    New file is created

--- src/HelloWorld1.java
+++ src/HelloWorld1.java
@@ -5,8 +5,13 @@ public class HelloWorld1 {
 
     // Doing awesome work 2 !
 
+
     // Doing awesome work 3 !
+
+    // Awesome work 3 is done and ready to be committed
+
     // any ready to be pushed to remote repository
+
 
 }
 
